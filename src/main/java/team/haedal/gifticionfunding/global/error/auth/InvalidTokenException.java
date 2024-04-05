146389726d96f24e8f102b0423803543df06bd93
Kwon Da Woon <momnpa333@gmail.com>
2024-04-06 03:15:15 +0900

    KNU-HAEDAL#1 feat: exception 처리

---
+++ src/main/java/team/haedal/gifticionfunding/global/error/auth/InvalidTokenException.java
@@ -0,0 +1,18 @@
+package team.haedal.gifticionfunding.global.error.auth;
+
+public class InvalidTokenException extends RuntimeException{
+    public InvalidTokenException() {
+        super();
+    }
+    public InvalidTokenException(String message) {
+        super(message);
+    }
+    public InvalidTokenException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public InvalidTokenException(Throwable cause) {
+        super(cause);
+    }
+
+
+}
