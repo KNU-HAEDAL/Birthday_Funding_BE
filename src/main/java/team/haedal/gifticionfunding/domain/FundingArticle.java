7aa954781a3ff11ca74ff258b55f79fc224706ed
kwonssshyeon <kwonsh4396@gmail.com>
2024-05-10 02:56:50 +0900

    #3 Feat: 펀딩 게시글 관련 엔티티 작성

--- src/main/java/team/haedal/gifticionfunding/domain/FundingArticle.java
+++ src/main/java/team/haedal/gifticionfunding/domain/FundingArticle.java
@@ -2,36 +2,58 @@ package team.haedal.gifticionfunding.domain;
 
 import jakarta.persistence.*;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
+@Getter
 @NoArgsConstructor
 public class FundingArticle {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "funding_id")
     private Long id;
     @ManyToOne()
     @OnDelete(action = OnDeleteAction.CASCADE)
     private Member member;
     private LocalDateTime createdAt;
-    private LocalDateTime endAt;
+    private LocalDate endAt;
     private String title;
     @Column(length = 50000)
     private String content;
+    @ManyToMany
+    @JoinTable(name = "funding_gifticon",
+            joinColumns = @JoinColumn(name = "funding_id"),
+            inverseJoinColumns = @JoinColumn(name = "gifticon_id"))
+    private List<Gifticon> gifticonList = new ArrayList<>();
 
     @Builder
-    private FundingArticle(Long id, Member member, LocalDateTime createdAt, LocalDateTime endAt, String title, String content){
+    private FundingArticle(Long id, Member member, LocalDateTime createdAt, LocalDate endAt, String title, String content, List<Gifticon> gifticonList){
         this.id = id;
         this.member = member;
         this.createdAt = createdAt;
         this.endAt = endAt;
         this.title = title;
         this.content = content;
+        this.gifticonList = gifticonList;
     }
 
-//    public static FundingArticle
+
+    public static FundingArticle of(Member member, LocalDate endAt, String title, String content, List<Gifticon> gifticonList){
+        return FundingArticle.builder()
+                .member(member)
+                .createdAt(LocalDateTime.now())
+                .endAt(endAt)
+                .title(title)
+                .content(content)
+                .gifticonList(gifticonList)
+                .build();
+    }
 }
--- src/main/java/team/haedal/gifticionfunding/domain/FundingGifticon.java
+++ src/main/java/team/haedal/gifticionfunding/domain/FundingGifticon.java
@@ -6,31 +6,15 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
-@Entity
-@NoArgsConstructor
+
 public class FundingGifticon {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @ManyToOne()
-    @OnDelete(action = OnDeleteAction.CASCADE)
-    private Gifticon gifticon;
-    @ManyToOne()
-    @OnDelete(action = OnDeleteAction.CASCADE)
-    private FundingArticle fundingArticle;
-
-    @Builder
-    private FundingGifticon(Long id, Gifticon gifticon, FundingArticle fundingArticle){
-        this.id = id;
-        this.fundingArticle = fundingArticle;
-        this.gifticon = gifticon;
-    }
-
-    public static FundingGifticon of(Gifticon gifticon, FundingArticle fundingArticle){
-        return FundingGifticon.builder()
-                .gifticon(gifticon)
-                .fundingArticle(fundingArticle)
-                .build();
-    }
-
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//    @ManyToOne()
+//    @OnDelete(action = OnDeleteAction.CASCADE)
+//    private Gifticon gifticon;
+//    @ManyToOne()
+//    @OnDelete(action = OnDeleteAction.CASCADE)
+//    private FundingArticle fundingArticle;
 }
