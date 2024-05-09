6a48b49b7fd1de73d12588db6aaa54477fbc5175
kwonssshyeon <kwonsh4396@gmail.com>
2024-05-09 16:27:15 +0900

    #2 Feat: 친구 관련 서비스 작성

--- src/main/java/team/haedal/gifticionfunding/controller/FriendController.java
+++ src/main/java/team/haedal/gifticionfunding/controller/FriendController.java
@@ -8,6 +8,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import team.haedal.gifticionfunding.dto.PageResponse;
+import team.haedal.gifticionfunding.dto.response.friend.FriendResponse;
+import team.haedal.gifticionfunding.service.FriendService;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,37 +18,43 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @RequestMapping("/api/friend")
 @Tag(name="Friend",description="친구 관련 API")
 public class FriendController {
+    //TODO: 친구 목록, 요청받은 친구 목록
+    //TODO: 친구 요청, 요청 수락, 요청 거절, 요청 취소
+    private final FriendService friendService;
+
     @GetMapping()
     @Operation(summary = "친구 목록 조회 API")
-    public ResponseEntity<?> getFriends(Authentication authentication){
-        return ResponseEntity.status(HttpStatus.OK).body(null);
+    public ResponseEntity<PageResponse<FriendResponse>> getFriends(Authentication authentication,
+                                                                   @RequestParam(value="page", defaultValue="0") int page){
+        return ResponseEntity.status(HttpStatus.OK).body(friendService.getFriends(page,authentication.getName()));
     }
 
     @PostMapping("/request")
     @Operation(summary = "친구 요청 API")
-    public ResponseEntity<?> requestFriend(Authentication authentication,
+    public void requestFriend(Authentication authentication,
                                            @RequestParam(name="friend") Long id){
-        return ResponseEntity.status(HttpStatus.CREATED).body(null);
+        System.out.println(authentication.getDetails().toString());
+        friendService.requestFriend(authentication.getName(),id);
     }
 
     @PostMapping("/accept")
     @Operation(summary = "친구 수락 API")
-    public ResponseEntity<?> acceptFriend(Authentication authentication,
+    public void acceptFriend(Authentication authentication,
                                           @RequestParam(name="friend") Long id){
-        return ResponseEntity.status(HttpStatus.CREATED).body(null);
-    }
+        friendService.acceptFriend(authentication.getName(),id);    }
 
     @PostMapping("/reject")
     @Operation(summary = "친구 거절 API")
-    public ResponseEntity<?> rejectFriend(Authentication authentication,
+    public void rejectFriend(Authentication authentication,
                                           @RequestParam(name="friend") Long id){
-        return ResponseEntity.status(HttpStatus.OK).body(null);
+        friendService.rejectFriend(authentication.getName(),id);
     }
 
     @GetMapping("/request")
-    @Operation(summary = "친구 요청 조회 API")
-    public ResponseEntity<?> getRequestedFriends(Authentication authentication){
-        return ResponseEntity.status(HttpStatus.OK).body(null);
+    @Operation(summary = "친구 요청 조회 API", description ="수락 대기중인 요청만 나타낸다.")
+    public ResponseEntity<PageResponse<FriendResponse>> getRequestedFriends(Authentication authentication,
+                                                                            @RequestParam(value="page", defaultValue="0") int page){
+        return ResponseEntity.status(HttpStatus.OK).body(friendService.getRequestedFriends(page, authentication.getName()));
     }
 
 
---
+++ src/main/java/team/haedal/gifticionfunding/service/FriendService.java
@@ -0,0 +1,90 @@
+package team.haedal.gifticionfunding.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import team.haedal.gifticionfunding.domain.Friendship;
+import team.haedal.gifticionfunding.domain.Member;
+import team.haedal.gifticionfunding.domain.enums.FriendStatus;
+import team.haedal.gifticionfunding.dto.PageResponse;
+import team.haedal.gifticionfunding.dto.response.friend.FriendResponse;
+import team.haedal.gifticionfunding.exception.CustomException;
+import team.haedal.gifticionfunding.exception.ErrorCode;
+import team.haedal.gifticionfunding.repository.dao.FriendOnly;
+import team.haedal.gifticionfunding.repository.FriendRepository;
+import team.haedal.gifticionfunding.repository.dao.MemberOnly;
+import team.haedal.gifticionfunding.repository.MemberRepository;
+
+import java.util.stream.Collectors;
+
+
+@Service
+@RequiredArgsConstructor
+public class FriendService {
+    private final MemberRepository memberRepository;
+    private final FriendRepository friendRepository;
+
+
+    @Transactional(readOnly = true)
+    public PageResponse<FriendResponse> getFriends(int page, String name){
+        Member member = memberRepository.getUserByEmail(name);
+        Page<FriendOnly> pageData = friendRepository.findAllByMemberAndStatus(PageRequest.of(page, 10), member, FriendStatus.ACCEPT);
+
+        return PageResponse.<FriendResponse>builder()
+                .content(pageData.getContent().stream()
+                        .map(data -> FriendResponse.from(data.getFriend()))
+                        .collect(Collectors.toList()))
+                .page(pageData.getNumber())
+                .isLast(pageData.isLast())
+                .build();
+    }
+
+
+    @Transactional()
+    public void requestFriend(String name, Long friendId){
+        Member sender = memberRepository.getUserByEmail(name);
+        Member receiver = memberRepository.getUserById(friendId);
+
+        friendRepository.save(Friendship.request(sender,receiver));
+    }
+
+
+    @Transactional()
+    public void acceptFriend(String name, Long friendId){
+        Member receiver = memberRepository.getUserByEmail(name);
+        Member sender = memberRepository.getUserById(friendId);
+        Friendship friendship = friendRepository.findByMemberAndFriend(sender, receiver)
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+        friendship.accept();
+        friendRepository.save(Friendship.finish(receiver,sender));
+    }
+
+
+    @Transactional()
+    public void rejectFriend(String name, Long friendId){
+        Member receiver = memberRepository.getUserByEmail(name);
+        Friendship friendship = friendRepository.findByMemberAndFriend(friendId, receiver.getId())
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+
+        // friendship에 접근할때 sender를 조회하기 위한 쿼리가 한번 더 나감 (N+1)
+        friendRepository.delete(friendship);
+    }
+
+
+    @Transactional(readOnly = true)
+    public PageResponse<FriendResponse> getRequestedFriends(int page, String name){
+        Member receiver = memberRepository.getUserByEmail(name);
+        Page<MemberOnly> pageData = friendRepository.findAllByFriendAndStatus(PageRequest.of(page, 10), receiver, FriendStatus.WAITING);
+
+        return PageResponse.<FriendResponse>builder()
+                .content(pageData.getContent().stream()
+                        .map(data -> FriendResponse.from(data.getMember()))
+                        .collect(Collectors.toList()))
+                .page(pageData.getNumber())
+                .isLast(pageData.isLast())
+                .build();
+    }
+
+}
