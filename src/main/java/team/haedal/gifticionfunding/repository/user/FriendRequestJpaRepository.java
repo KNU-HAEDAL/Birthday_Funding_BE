6a7a6e43a81362c8b6734ca016d6a369023b1476
momnpa333 <momnpa333@gmail.com>
2024-05-11 02:03:42 +0900

    KNU-HAEDAL#2 feat: FriendRequestJpaRepository 페이지 메서드 생성

--- src/main/java/team/haedal/gifticionfunding/repository/user/FriendRequestJpaRepository.java
+++ src/main/java/team/haedal/gifticionfunding/repository/user/FriendRequestJpaRepository.java
@@ -1,7 +1,11 @@
 package team.haedal.gifticionfunding.repository.user;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import team.haedal.gifticionfunding.entity.user.FriendRequest;
 
 public interface FriendRequestJpaRepository extends JpaRepository<FriendRequest, Long> {
+    Page<FriendRequest> findAllByRequesterId(Long requesteeId, Pageable pageable);
+    Page<FriendRequest> findAllByRequesteeId(Long requesterId, Pageable pageable);
 }
